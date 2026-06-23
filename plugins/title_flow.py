b3c35c33c0ff6cc036a5efa9e20e4b01accf3217
sᴜʙᴀʀᴜ <rajibsarkar986431@gmail.com>
2026-06-23 12:14:50 +0530

    Update title_flow.py

--- plugins/title_flow.py
+++ plugins/title_flow.py
@@ -44,8 +44,10 @@ async def ask_poster_choice(client, message, session):
     await client.send_message(
         chat_id=message.chat.id,
         text="<b><blockquote>⟡ TITLE CHOICE ❞</b></blockquote>"
-"<b><blockquote> \n◇ Would you like to use the official title from AniList/TMDb, or set your own custom title? ❞
+"""
+<b><blockquote>\n◇ Would you like to use the official title from AniList/TMDb, or set your own custom title? ❞
 \n◇ (This title will appear on the poster and in the caption) ❞
 \n◇ Query : session.selected["title"]❞?</b></blockquote>",
         reply_markup=markup,
     )
+"""
